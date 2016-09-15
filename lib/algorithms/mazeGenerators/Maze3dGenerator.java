1bb60de35dccf393eda488d96b61e7620dd29ad2
Tom Shabtay <tomshabtay@Toms-MacBook-Pro.local>
2016-09-16 00:14:27 +0300

    1

--- lib/algorithms/mazeGenerators/Maze3dGenerator.java
+++
@@ -1,9 +0,0 @@
-package algorithms.mazeGenerators;
-
-public interface Maze3dGenerator {
-
-	public Maze3d generate(int x, int y, int z);
-
-	public String measureAlgorithmTime(int x, int y, int z);
-
-}
--- lib/algorithms/mazeGenerators/Updateable.java
+++
@@ -1,7 +0,0 @@
-package algorithms.mazeGenerators;
-
-public interface Updateable {
-
-	public void update(Cell3D temp, Cell3D n);
-
-}
